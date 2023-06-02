ed1fbce0834d05d2c6baacd0808505225a3a8aeb
elentz <eric.lentz@softwareprophets.com>
2023-06-01 20:17:56 -0400

    Initial code deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>End of Day</h1>
       </header>
+      <ul>
+        <li>--- Confidential -------</li>
+        <li>--- Confidential -------</li>
+        <li>--- Confidential -------</li>
+        <li>--- Confidential -------</li>
+      </ul>
     </div>
   );
 }
